a1d7dd0baddf8873399f2b77e4da3b90b18b1c77
vpn12out <88290750+vpn12out@users.noreply.github.com>
2021-11-22 22:24:31 +0530

    hi

--- tortoolkit/core/getVars.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-# (c) YashDK [yash-dk@github]
-
-# from ..core.database_handle import TorToolkitDB
-from tortoolkit import SessionVars
-
-
-def get_val(variable):
-    return SessionVars.get_var(variable)
